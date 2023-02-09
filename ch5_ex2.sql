2e325ad6b1b9ab54d2cf381d4105122cd37c2f3c
Harrisonjstreet <64044958+Harrisonjstreet@users.noreply.github.com>
2023-02-08 19:52:07 -0500

    Add files via upload

---
+++ ch5_ex2.sql
@@ -0,0 +1,7 @@
+SELECT * FROM terms;
+INSERT INTO terms (terms_id, terms_description, terms_due_days)
+VALUES (6, 'Net due 120 days', 120);
+
+UPDATE terms
+SET terms_description = 'Net due 125 days', terms_due_days = 125
+WHERE terms_id = 6;
